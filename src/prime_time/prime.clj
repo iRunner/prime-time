fecb8770fbbe498d9d4616f26f4a3e00b3d61d58
Abdullah Sahyouni <abdullahsahyouni@gmail.com>
2017-06-09 17:12:26 -0400

    Added small set of documentation

--- src/prime_time/prime.clj
+++ src/prime_time/prime.clj
@@ -2,8 +2,20 @@
 
 (def certainty 5)
 
-(defn prime? [n]
+(defn prime? 
+  "
+    input: integer
+    output: true or false depending on if integer is prime
+
+    problem: maynot work for integers greater than a large number
+  "
+  [n]
   (.isProbablePrime (BigInteger/valueOf n) certainty))
 
-(defn first-n-primes [n]
+(defn first-n-primes 
+  "
+    input: integer
+    output: array of n size of prime numbers starting with 2
+  "
+  [n]
   (take n (filter prime? (iterate inc 1))))
--- src/prime_time/table.clj
+++ src/prime_time/table.clj
@@ -1,12 +1,26 @@
 (ns prime-time.table
   (:require [prime-time.prime :as prime]))
 
-(defn fill-row [front arr]
+(defn fill-row 
+  "input: 
+    front: first coloumn in row/number to multiply array by
+    arr:  rest of row 
+  return: a row in the table in a form of a map (keys are prime numbers)
+          {0 front} is the first column
+  * did this because wanted to utilize clojure.pprint/print-table :D
+  "
+  [front arr]
   (let [multi (map #(* front %) arr)
         multi-map (into {0 front} (zipmap arr multi))]
     (into (sorted-map) multi-map)))
 
 
-(defn create-table [n]
+(defn create-table
+  "
+    input: table size (n*n)
+    output: array of rows that are beautifully printed by 
+    clojure.pprint/print-table
+  " 
+  [n]
   (let [n-prime (prime/first-n-primes n)]
     (map #(fill-row % n-prime) n-prime)))
